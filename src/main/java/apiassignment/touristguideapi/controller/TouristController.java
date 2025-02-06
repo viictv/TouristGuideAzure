0fa3a7dc14880c333317e8e5e91856646404415f
Zahaawii <zaak@kromannreumert.com>
2025-02-06 16:58:47 +0100

    adding methods

--- src/main/java/apiassignment/touristguideapi/controller/TouristController.java
+++ src/main/java/apiassignment/touristguideapi/controller/TouristController.java
@@ -5,10 +5,7 @@ import apiassignment.touristguideapi.service.TouristService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,7 +19,7 @@ private final TouristService touristService;
         this.touristService = touristService;
     }
 
-    @GetMapping("/test")
+    @GetMapping("/attractions")
     public ResponseEntity<List<TouristAttraction>> getAllAttractions() {
     List<TouristAttraction> t1 = touristService.getAllAttractions();
     return new ResponseEntity<>(t1, HttpStatus.OK);
@@ -34,4 +31,26 @@ private final TouristService touristService;
         return new ResponseEntity<>(t1,HttpStatus.OK);
     }
 
-}
+    @PostMapping("/add")
+    public ResponseEntity<TouristAttraction> addAttraction(@RequestBody TouristAttraction t1) {
+        TouristAttraction addAttraction = touristService.addTouristAttraction(t1);
+        return new ResponseEntity<>(addAttraction, HttpStatus.CREATED);
+        }
+
+    @PostMapping("/update")
+    public ResponseEntity<TouristAttraction> renameAttraction(@RequestBody String name, String replacementname) {
+        TouristAttraction test = touristService.renameAttraction(name, replacementname);
+        return new ResponseEntity<>(test, HttpStatus.OK);
+    }
+
+    @PostMapping("/delete/{name}")
+    public ResponseEntity<TouristAttraction> removeAttraction(@PathVariable String name) {
+        TouristAttraction test = touristService.removeAttraction(name);
+        return new ResponseEntity<>(test, HttpStatus.OK);
+    }
+
+
+    }
+
+
+
